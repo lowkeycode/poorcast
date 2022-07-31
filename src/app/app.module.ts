39ece12fcfb0b07eb6fbb92cd62a56bb2790215c
Cam Remesz <saltyndead@gmail.com>
2022-07-31 15:34:52 -0600

    get ui skeleton of login page up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInPageComponent } from './components/sign-in-page/sign-in-page.component';
+import { LogoComponent } from './components/ui/logo/logo.component';
+import { SignInComponent } from './components/sign-in-page/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-in-page/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInPageComponent,
+    LogoComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
