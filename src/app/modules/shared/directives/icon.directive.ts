8efb2cca1c2d8ce93863d53e86e75ad797ae1c0e
Cam Remesz <saltyndead@gmail.com>
2022-09-14 23:40:15 -0600

    Remove unused imports

--- src/app/modules/shared/directives/icon.directive.ts
+++ src/app/modules/shared/directives/icon.directive.ts
@@ -1,11 +1,11 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostBinding, Input } from '@angular/core';
+import { Directive, OnInit, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appIconSize]'
 })
 export class IconDirective implements OnInit {
   @Input('appIconSize') remSize = 1;
-  
+
   @HostBinding('style.width') width: string;
   @HostBinding('style.height') height: string;
 
