9be222fb94f7ce92d5268e8c3bb31fd737b3beff
Cam Remesz <saltyndead@gmail.com>
2022-09-20 22:30:39 -0600

    Implement responsive design for sign in page

--- src/app/modules/user/components/sign-in-page/sign-in-page.component.ts
+++ src/app/modules/user/components/sign-in-page/sign-in-page.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
-  styleUrls: ['./sign-in-page.component.scss']
+  styleUrls: ['./sign-in-page.component.scss'],
 })
 export class SignInPageComponent {
+  constructor() {}
 
 }
