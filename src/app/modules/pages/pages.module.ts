ec5143606feed873db3ef94242335f5607339b3c
Cam Remesz <saltyndead@gmail.com>
2022-12-07 08:09:15 -0500

    Changed bills to budget

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -2,30 +2,17 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverviewComponent } from './overview/overview.component';
-import { BillsComponent } from './bills/bills.component';
+import { BudgetComponent } from './bills/budget.component';
 import { SettingsComponent } from './settings/settings.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { UpcomingCardComponent } from './overview/upcoming-card/upcoming-card.component';
 
-
-const components = [
-  OverviewComponent,
-  BillsComponent,
-  SettingsComponent,
-]
+const components = [OverviewComponent, BudgetComponent, SettingsComponent, UpcomingCardComponent];
 
 @NgModule({
-  declarations: [
-    ...components,
-    UpcomingCardComponent,
-  ],
-  imports: [
-    CommonModule,
-    NgxChartsModule,
-    SharedModule
-  ],
-  exports: [
-    ...components
-  ]
+  declarations: [...components],
+  imports: [CommonModule, NgxChartsModule, SharedModule],
+  exports: [...components],
 })
-export class PagesModule { }
+export class PagesModule {}
+  
