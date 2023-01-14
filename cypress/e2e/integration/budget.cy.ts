116f0b84365ad8524c30bf0f4c0edc0ccea8100b
Cam Remesz <saltyndead@gmail.com>
2023-01-13 22:16:12 -0500

    Finish budget unit tests

--- cypress/e2e/integration/budget.cy.ts
+++ cypress/e2e/integration/budget.cy.ts
@@ -13,9 +13,44 @@ describe('Budget', () => {
     cy.get('.accts').should('be.visible')
   })
 
-  it('shows account modal on add account click', () => {
+  it('closes modal on modal close click', () => {
+    cy.get('[data-test="add-acct-btn"]').click()
+    cy.get('.modal__main--title').should('be.visible').contains('Add Account')
+    cy.get('.modal__main--close').click()
+    cy.get('.modal').should('not.exist')
+  })
+
+  it('shows add account modal on add account click', () => {
     cy.get('[data-test="add-acct-btn"]').click()
     cy.get('.modal__main--title').should('be.visible').contains('Add Account')
   })
 
+  it('shows edit modal on edit account click', () => {
+    cy.get('[data-test="edit-acct-btn-0"]').click()
+    cy.get('.modal__main--title').should('be.visible').contains('Edit Account')
+  })
+
+  it('shows transfer modal on transfer click', () => {
+    cy.get('[data-test="transfer-btn"]').click()
+    cy.get('.modal__main--title').should('be.visible').contains('Transfer')
+  })
+
+  it('renders bills section', () => {
+    cy.get('.bills__table--header').should('be.visible')
+  })
+
+  it('shows pay bill modal on pay bill click', () => {
+    cy.get('[data-test="pay-bill-btn"]').click()
+    cy.get('.modal__main--title').should('be.visible').contains('Pay Bill')
+  })
+
+  it('shows add bill modal on add bill click', () => {
+    cy.get('[data-test="add-bill-btn"]').click()
+    cy.get('.modal__main--title').should('be.visible').contains('Add Bill')
+  })
+
+  it('shows edit bill modal on edit bill click', () => {
+    cy.get('[data-test="edit-bill-btn-0"]').click()
+    cy.get('.modal__main--title').should('be.visible').contains('Edit Bill')
+  })
 }) 
