352ff17b6718f01eb6b49b800a88a4fac79f11d1
Cam Remesz <saltyndead@gmail.com>
2023-02-16 09:07:20 -0500

    Finish UI

--- src/app/modules/pages/settings/settings.component.ts
+++ src/app/modules/pages/settings/settings.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+type DeleteStates = 'delete' | 'confirm' | 'success';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent {
+  deleteState: DeleteStates = 'delete';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onDelete() {
+    this.deleteState = 'confirm';
   }
 
+  onConfirm() {
+    this.deleteState = 'success';
+  }
+
+  onCancel() {
+    this.deleteState = 'delete';
+  }
 }
