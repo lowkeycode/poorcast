86e80b2259f03d1ce42a97226298d7eba85ae130
Cam Remesz <saltyndead@gmail.com>
2023-10-25 21:33:24 -0400

    Uncomment auth guard

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   {
     path: 'app',
     component: NavWrapComponent,
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'overview',
