a230d50679065b5ae02fa988efa037330d3ef939
Cam Remesz <saltyndead@gmail.com>
2023-10-29 13:30:58 -0400

    Update to angular 16

--- src/app/modules/user/guards/auth.guard.ts
+++ src/app/modules/user/guards/auth.guard.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { map, Observable, of, take, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private pcAuth: AuthService, private router: Router) {}
   canActivate(): Observable<boolean | UrlTree> {
     return this.pcAuth.user.pipe(
