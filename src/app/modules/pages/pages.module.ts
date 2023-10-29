8c2087c98582dbae5d6f18d40a2dbdf472e4edda
Cam Remesz <saltyndead@gmail.com>
2023-10-29 14:12:58 -0400

    Fuck ngx-charts on outta here

--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -4,11 +4,9 @@ import { CommonModule } from '@angular/common';
 import { OverviewComponent } from './overview/overview.component';
 import { BudgetComponent } from './budget/budget.component';
 import { SettingsComponent } from './settings/settings.component';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { UpcomingCardComponent } from './overview/upcoming-card/upcoming-card.component';
 import { BudgetAcctsComponent } from './budget/budget-accts/budget-accts.component';
 import { ExpensesComponent } from './budget/expenses/expenses.component';
-import { TuiCalendarRangeModule } from '@taiga-ui/kit';
 
 const components = [
   OverviewComponent,
@@ -23,9 +21,7 @@ const components = [
   declarations: [...components],
   imports: [
     CommonModule,
-    NgxChartsModule,
     SharedModule,
-    TuiCalendarRangeModule,
   ],
   exports: [...components],
 })
