a083e384e0f6c34cee0acffc7ee96d11d9241d27
Cam Remesz <saltyndead@gmail.com>
2024-01-24 09:30:24 -0500

    WIP - Reading up on dynamic reactive forms because I built my own and I dont know if the architecture will work...frick

--- src/app/modules/shared/components/modal/modal.component.ts
+++ src/app/modules/shared/components/modal/modal.component.ts
@@ -1,6 +1,7 @@
 import { ModalService } from './../../services/modal.service';
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
 import { ModalConfig } from 'src/app/models/interfaces';
 
 
@@ -9,16 +10,33 @@ import { ModalConfig } from 'src/app/models/interfaces';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
   modal$: Observable<ModalConfig | null>;
+  form!: UntypedFormGroup;
+  private subs = new Subscription();
 
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private fb: UntypedFormBuilder) {}
 
   ngOnInit(): void {
     this.modal$ = this.modalService.modalState$;
+    const modalSub = this.modal$.subscribe(modal => {
+      console.log('modal', modal)
+    });
+    this.subs.add(modalSub);
+
+    this.form = this.fb.group({})
+    
+  }
+
+  ngOnDestroy(): void {
+      this.subs.unsubscribe();
   }
 
   closeModal() {
     this.modalService.closeModal();
+  }
+
+  onSubmit() {
+    console.log('submit')
   }
 }
